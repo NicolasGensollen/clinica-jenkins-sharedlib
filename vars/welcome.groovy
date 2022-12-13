03ae82130990501ca144128ccb3535fe22decce2
NicolasGensollen <nicolas.gensollen@gmail.com>
2022-12-13 15:56:44 +0100

    Add complexity

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,3 +1,6 @@
 def call(Map config = [:]){
 	echo "Hello ${config.name}. Today is ${config.dayOfWeek}"
+	echo "We will create a folder for you at ${config.homeFolder}"
+	sh "mkdir ${config.homeFolder}"
+	sh "ls"
 }
